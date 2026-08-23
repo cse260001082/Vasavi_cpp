22aead91b64401dbb3479eb54051e8a8bd4a7ba8
dineshydk <dineshkrishnayeturi@gmail.com>
2026-08-23 16:58:33 +0530

    Add Palindrome code and MiniTest Question

---
+++ MiniTest.cpp
@@ -0,0 +1,70 @@
+/*\
+Write one complete C++ program that takes an integer such as: 58342
+
+and prints:
+
+Number: 58342
+Reversed: 24385
+Sum of digits: 22
+Total Digits: 5
+Even digits: 3
+Odd digits: 2
+
+Try to use at least 2 separate functions.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void digitDetail(int n){
+    //  Write the whole code logic to print these 3 lines..
+        // Total Digits: 5
+        // Even digits: 3
+        // Odd digits: 2
+
+    
+}
+
+int sumDigits(int n){
+    int sum = 0;
+    // sum = sum + 
+    while(n / 10 == 0){
+        sum = sum + n%10;
+        n = n/10;
+    }
+
+    return sum;
+}
+
+int reversedNum(int n){
+
+    // Write code again here... with own, without any reference code..
+}
+
+int main()  {
+    int n;
+    cin >> n;
+
+    cout<<"Number: " << n;
+    cout<<"Reversed Number: "<<reversedNum(n);
+    cout<<"Sum of Digits: " << sumDigits(n);
+    digitDetail(n);
+    return 0;
+}
+
+/*
+Write comments, when you are done with your code.. and 
+also run these test cases and check your code
+|    Input | Reversed |  Sum | Total Digits | Even Digits | Odd Digits |
+| -------: | -------: | ---: | -----------: | ----------: | ---------: |
+|   58342  |   24385  |  22  |           5  |          3  |         2  |
+|   12345  |   54321  |  15  |           5  |          2  |         3  |
+|    2468  |    8642  |  20  |           4  |          4  |         0  |
+|   13579  |   97531  |  25  |           5  |          0  |         5  |
+|       7  |       7  |   7  |           1  |          0  |         1  |
+|       8  |       8  |   8  |           1  |          1  |         0  |
+|    1002  |    2001  |   3  |           4  |          3  |         1  |
+|    1200  |      21  |   3  |           4  |          3  |         1  |
+|  999999  |  999999  |  54  |           6  |          0  |         6  |
+|  808080  |   80808  |  24  |           6  |          6  |         0  |
+*/ 
--- Palindrome.cpp
+++ Palindrome.cpp
@@ -5,7 +5,7 @@ Question:
 
     *Palindome*
         It is a number which gives same when reading from either of the sides
-    1221 -> Palindrome
+    1221 -> Palindrome (What is the reverse of this number = 1221)
     1234 -> Not Palindrome
 */
 
@@ -17,6 +17,22 @@ int main()
     int n;
     cout << "Enter a number to identify it's palindrome nature\n";
     cin >> n;
+    int rev = 0;
+    int temp = n;
 
+    while (temp > 0)
+    {
+        rev = rev * 10 + (temp % 10);
+        temp /= 10;
+    }
+
+    if (rev == n)
+    {
+        cout << "The number " << n << " is a palindrome";
+    }
+    else
+    {
+        cout << "The number " << n << " is not a palindrome";
+    }
     return 0;
 }
