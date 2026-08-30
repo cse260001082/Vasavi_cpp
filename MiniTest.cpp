a57de559a0451767f649caa9cbd4b4888d503d92
Your Full Name <your-email@example.com>
2026-08-30 18:09:28 +0530

    Finished DigitDetail Function in miniTest.cpp

--- MiniTest.cpp
+++ MiniTest.cpp
@@ -16,44 +16,68 @@ Try to use at least 2 separate functions.
 #include <bits/stdc++.h>
 using namespace std;
 
-void digitDetail(int n){
+void digitDetail(int n)
+{
     //  Write the whole code logic to print these 3 lines..
-        // Total Digits: 5
-        // Even digits: 3
-        // Odd digits: 2
+    // Total Digits: 5
+    // Even digits: 3
+    // Odd digits: 2
+    int count = 0;
+    int even_count = 0;
+    int odd_count = 0;
 
-    
+    while (n != 0)
+    {
+        count = count + 1;
+
+        if (n % 2 == 0)
+        {
+            even_count = even_count + 1;
+        }
+        else
+        {
+            odd_count = odd_count + 1;
+        }
+        n = n / 10;
+    }
+    cout << "Total Digits: " << count << endl;
+    cout << "even_count: " << even_count << endl;
+    cout << "odd_count: " << odd_count << endl;
 }
 
-int sumDigits(int n){
+int sumDigits(int n)
+{
     int sum = 0;
-    // sum = sum + 
-    while(n / 10 == 0){
-        sum = sum + n%10;
-        n = n/10;
+    // sum = sum +
+    while (n / 10 == 0)
+    {
+        sum = sum + n % 10;
+        n = n / 10;
     }
 
     return sum;
 }
 
-int reversedNum(int n){
+int reversedNum(int n)
+{
 
     // Write code again here... with own, without any reference code..
 }
 
-int main()  {
+int main()
+{
     int n;
     cin >> n;
 
-    cout<<"Number: " << n;
-    cout<<"Reversed Number: "<<reversedNum(n);
-    cout<<"Sum of Digits: " << sumDigits(n);
+    cout << "Number: " << n << endl;
+    // cout << "Reversed Number: " << reversedNum(n);
+    // cout << "Sum of Digits: " << sumDigits(n);
     digitDetail(n);
     return 0;
 }
 
 /*
-Write comments, when you are done with your code.. and 
+Write comments, when you are done with your code.. and
 also run these test cases and check your code
 |    Input | Reversed |  Sum | Total Digits | Even Digits | Odd Digits |
 | -------: | -------: | ---: | -----------: | ----------: | ---------: |
@@ -67,4 +91,4 @@ also run these test cases and check your code
 |    1200  |      21  |   3  |           4  |          3  |         1  |
 |  999999  |  999999  |  54  |           6  |          0  |         6  |
 |  808080  |   80808  |  24  |           6  |          6  |         0  |
-*/ 
+*/
